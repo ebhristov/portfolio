05c9ccf757a0dda96dfc6d13f36327245c0cc42a
Evlogi Hristov <ebhristov@gmail.com>
2024-06-08 00:06:59 +0300

    made a clock component

---
+++ resources/js/Layouts/Clock.vue
@@ -0,0 +1,152 @@
+<script setup>
+import {ref, onMounted} from 'vue';
+
+const hourHand = ref(null);
+const minuteHand = ref(null);
+const secondHand = ref(null);
+
+function clockTick() {
+    const date = new Date();
+    const seconds = date.getSeconds();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+
+    const totalSeconds = hours * 3600 + minutes * 60 + seconds;
+
+    const secondsFraction = totalSeconds / 60;
+    const minutesFraction = totalSeconds / 3600;
+    const hoursFraction = totalSeconds / 43200;
+
+    rotateClockHand(secondHand.value, secondsFraction);
+    rotateClockHand(minuteHand.value, minutesFraction);
+    rotateClockHand(hourHand.value, hoursFraction);
+}
+
+function rotateClockHand(element, rotation) {
+    if (element) {
+        element.style.transform = `translate(-50%) rotate(${rotation * 360}deg)`;
+    }
+}
+
+function startClock() {
+    clockTick();
+    requestAnimationFrame(startClock);
+}
+
+onMounted(() => {
+    startClock();
+});
+</script>
+
+<template>
+    <div class="absolute w-screen h-screen flex justify-center items-center select-none">
+        <div class="clock">
+            <div
+                v-for="n in 12"
+                :key="'hour-' + n"
+                class="number"
+                :style="{ transform: `rotate(${30 * n}deg)` }"
+            >
+                <span :style="{ transform: `rotate(${-30 * n}deg)` }">{{ n }}</span>
+            </div>
+            <div
+                v-for="m in 60"
+                :key="'minute-' + m"
+                class="minute-number"
+                :style="{ transform: `rotate(${6 * m}deg)` }"
+            >
+                <span>{{ m }}</span>
+            </div>
+            <div class="hour-hand" ref="hourHand"></div>
+            <div class="minute-hand" ref="minuteHand"></div>
+            <div class="second-hand" ref="secondHand"></div>
+            <div class="center-dot"></div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.clock {
+    position: relative;
+    width: 50vw;
+    height: 50vw;
+    border: 1vw solid rgb(0, 255, 255);
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+    z-index: 1;
+}
+
+.number {
+    position: absolute;
+    text-align: center;
+    inset: 3%;
+    color: rgba(0, 255, 255, 0.8);
+    transform-origin: center;
+    font-family: Bahnschrift, sans-serif;
+    font-weight: bold;
+    font-size: 6vw;
+}
+
+.number > span {
+    display: inline-block;
+}
+
+.minute-number {
+    position: absolute;
+    text-align: center;
+    inset: 1.5%;
+    color: rgba(255, 0, 0, 0.8);
+    transform-origin: center;
+    font-family: Bahnschrift, sans-serif;
+    font-size: 1vw;
+}
+
+.minute-number > span {
+    display: inline-block;
+}
+
+.center-dot {
+    position: absolute;
+    width: 2vw;
+    height: 2vw;
+    background-color: black;
+    background-clip: padding-box;
+    border: 0.5vw solid rgba(255, 0, 0, 0.8);
+    border-radius: 50%;
+    z-index: 10;
+}
+
+.hour-hand, .minute-hand, .second-hand {
+    position: absolute;
+    left: 50%;
+    bottom: 50%;
+    transform-origin: bottom;
+    border-radius: 5px;
+    z-index: 5;
+    transition: transform 0.5s cubic-bezier(0.4, 2.3, 0.3, 1);
+}
+
+.hour-hand {
+    width: 1.5%;
+    height: 30%;
+    background-color: rgba(0, 255, 255, 0.8);
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+}
+
+.minute-hand {
+    width: 1.2%;
+    height: 40%;
+    background-color: rgba(0, 255, 255, 0.8);
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+}
+
+.second-hand {
+    width: 0.8%;
+    height: 45%;
+    background-color: rgba(255, 0, 0, 0.8);
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+}
+</style>
--- resources/js/Layouts/DefaultLayout.vue
+++ resources/js/Layouts/DefaultLayout.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
  import Header from "./Header.vue";
+ import Clock from "@/Layouts/Clock.vue";
 </script>
 
 <template>
     <main class="h-screen bg-gradient-to-b from-black from-60% to-red-900 text-white flex flex-col">
         <Header/>
+        <Clock/>
         <div class="container bg-gray-500/25 h-full rounded-2xl shadow-2xl my-5">
             <slot/>
         </div>
