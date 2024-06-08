c89ff1a82321e4084dda3a13dbadeae9779be645
Evlogi Hristov <ebhristov@gmail.com>
2024-06-08 17:01:00 +0300

    ui setup

--- resources/js/Pages/Library.vue
+++ resources/js/Pages/Library.vue
@@ -1,12 +1,19 @@
 <script setup>
-
 import {Head} from '@inertiajs/vue3';
 </script>
 
 <template>
     <Head title="Library"/>
+    <div class="flex flex-col h-full w-full select-none overflow-hidden md:flex-row">
+        <aside class="w-full p-5 flex flex-col gap-5 place-items-center align-middle md:w-96">
+            <h1 class="text-4xl">Library</h1>
+            <p>This library project has the purpose of demonstrating crud, generation of data, pagination and fast
+                search through meilisearch.</p>
+        </aside>
+        <div class="w-full flex place-content-center items-center p-5">
 
-    library project
+        </div>
+    </div>
 </template>
 
 <style scoped>
